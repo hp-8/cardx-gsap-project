96d4701bab6a60cfba331c04e98806173a9c855e
Harsh Patadia <patadiaharsh.8@gmail.com>
2024-08-04 12:38:56 +0530

    minor changes

--- src/components/UI/Footer/index.tsx
+++ src/components/UI/Footer/index.tsx
@@ -75,7 +75,7 @@ const Footer = () => {
           </FooterMiddle>
           <FooterBottom>
             <Translator>
-              <h3>English (United Kingdom)</h3>
+              <h3>English (India)</h3>
               <Image src={ic_chevron_down} alt="chevron down" />
             </Translator>
             <CopyRight>
